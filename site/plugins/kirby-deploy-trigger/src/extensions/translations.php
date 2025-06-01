6d353fc42b025692f48e2d0eafb46e96d0b4da33
parislettau <55466163+parislettau@users.noreply.github.com>
2025-06-01 19:47:31 +1000

    composer require johannschopplich/kirby-deploy-trigger

---
+++ site/plugins/kirby-deploy-trigger/src/extensions/translations.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'en' => [
+        'johannschopplich.deploy-trigger.label' => 'Deploy Site',
+        'johannschopplich.deploy-trigger.success' => 'Successfully triggered deployment'
+    ],
+    'de' => [
+        'johannschopplich.deploy-trigger.label' => 'Seite deployen',
+        'johannschopplich.deploy-trigger.success' => 'Deployment erfolgreich gestartet'
+    ],
+    'fr' => [
+        'johannschopplich.deploy-trigger.label' => 'Déployer le site',
+        'johannschopplich.deploy-trigger.success' => 'Déploiement démarré avec succès'
+    ],
+    'nl' => [
+        'johannschopplich.deploy-trigger.label' => 'Site implementeren',
+        'johannschopplich.deploy-trigger.success' => 'Implementatie succesvol gestart'
+    ]
+];

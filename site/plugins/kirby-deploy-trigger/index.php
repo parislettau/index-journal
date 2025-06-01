0f4ba8903c7881d111d49dad75c496c8119facc0
parislettau <55466163+parislettau@users.noreply.github.com>
2025-06-01 20:42:39 +1000

    reove deploy

--- site/plugins/kirby-deploy-trigger/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Kirby\Cms\App;
-
-App::plugin('johannschopplich/deploy-trigger', [
-    'api' => require __DIR__ . '/src/extensions/api.php',
-    'translations' => require __DIR__ . '/src/extensions/translations.php'
-]);

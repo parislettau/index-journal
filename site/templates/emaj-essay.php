7c33d3676bc34e95b12f9c55c8e142d8a7a1ec99
Paris Lettau <55466163+parislettau@users.noreply.github.com>
2025-06-08 16:58:25 +1000

    fix

--- site/templates/emaj-essay.php
+++ site/templates/emaj-essay.php
@@ -4,7 +4,7 @@
 <main data-template="<?= $page->template() ?>">
   <article>
     <header>
-      <h1><span class="title"><?= $page->title() ?></span>
+      <h1 style="display:block"><span class="title"><?= $page->title() ?></span>
         <?php if ($page->subtitle()->isNotEmpty()) : ?><span class="subtitle"><?= smartypants($page->subtitle()->kti()) ?></span><?php endif ?>
         <?php if ($page->slug() != 'introduction') : ?><span class="author">by <?= $page->author() ?></span><?php endif ?>
       </h1>
@@ -14,22 +14,22 @@
 
       <div class="text-block">
 
-    <?php if ($page->abstract()->isNotEmpty()) : ?>
-      <div class="abstract-block">
-        <div class="abstract-content">
-          <div class="shadow"></div>
-          <div class="abstract hidden"><?= smartypants($page->abstract()->kirbytext()) ?></div>
-        </div>
-      </div>
-    <?php endif ?>
+        <?php if ($page->abstract()->isNotEmpty()) : ?>
+          <div class="abstract-block">
+            <div class="abstract-content">
+              <div class="shadow"></div>
+              <div class="abstract hidden"><?= smartypants($page->abstract()->kirbytext()) ?></div>
+            </div>
+          </div>
+        <?php endif ?>
 
-    <?= smartypants($page->text()->kirbytext()) ?>
-    <div class="text-bios">
-      <?= smartypants($page->bios()->kirbytext()) ?>
-    </div>
-    <div class="text-bibliography">
-      <?= smartypants($page->bibilography()->kirbytext()) ?>
-    </div>
+        <?= smartypants($page->text()->kirbytext()) ?>
+        <div class="text-bios">
+          <?= smartypants($page->bios()->kirbytext()) ?>
+        </div>
+        <div class="text-bibliography">
+          <?= smartypants($page->bibilography()->kirbytext()) ?>
+        </div>
 
 
     </section>
@@ -54,4 +54,5 @@
 
 </main>
 <?php snippet('footer') ?>
+
 </html>

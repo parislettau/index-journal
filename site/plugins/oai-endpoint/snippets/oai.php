eb6bd6658959a5ae6b55432ba0fc76a5c4d47be3
Paris Lettau <55466163+parislettau@users.noreply.github.com>
2025-06-09 18:32:06 +1000

    Update oai.php

--- site/plugins/oai-endpoint/snippets/oai.php
+++ site/plugins/oai-endpoint/snippets/oai.php
@@ -15,9 +15,9 @@ foreach ($issues as $issue) {
         'creators' => $editors,
         'doi'      => $issue->issue_doi()->value(),
         'abstract' => null,
-        'url'      => $issue->url(),
+        'url'      => $site->url() . '/' . $issue->id(),
         'subject'  => $issue->keywords()->value(),
-        'publisher'=> 'Index Journal',
+        'publisher' => 'Index Journal',
         'pubDate'  => $issue->issue_date()->toDate('Y-m-d'),
         'type'     => 'Issue',
         'language' => 'en',
@@ -38,7 +38,7 @@ foreach ($issues as $issue) {
             'abstract' => $essay->abstract()->value(),
             'url'      => $essay->url(),
             'subject'  => $essay->keywords()->value(),
-            'publisher'=> $essay->publisher()->or('Index Journal')->value(),
+            'publisher' => $essay->publisher()->or('Index Journal')->value(),
             'pubDate'  => $essay->parent()->issue_date()->toDate('Y-m-d'),
             'type'     => $essay->article_type()->or('ScholarlyArticle')->value(),
             'language' => $essay->language()->or('en')->value(),
@@ -54,39 +54,49 @@ foreach ($issues as $issue) {
     <responseDate><?= gmdate('Y-m-d\TH:i:s\Z'); ?></responseDate>
     <request verb="ListRecords" metadataPrefix="oai_dc"><?= $site->url() ?>/oai</request>
     <ListRecords>
-<?php foreach ($records as $r): ?>
-        <record>
-            <header>
-                <identifier><?= esc($r['id']) ?></identifier>
-                <datestamp><?= esc($r['date']) ?></datestamp>
-            </header>
-            <metadata>
-                <oai_dc:dc xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/"
-                           xmlns:dc="http://purl.org/dc/elements/1.1/"
-                           xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                           xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd">
-                    <dc:title><?= esc($r['title']) ?></dc:title>
-<?php foreach ($r['creators'] as $c): ?>
-                    <dc:creator><?= esc($c) ?></dc:creator>
-<?php endforeach; ?>
-<?php if (!empty($r['subject'])): ?>
-                    <dc:subject><?= esc($r['subject']) ?></dc:subject>
-<?php endif; ?>
-                    <dc:publisher><?= esc($r['publisher']) ?></dc:publisher>
-                    <dc:date><?= esc($r['pubDate']) ?></dc:date>
-                    <dc:type><?= esc($r['type']) ?></dc:type>
-                    <dc:language><?= esc($r['language']) ?></dc:language>
-                    <dc:rights><?= esc($r['rights']) ?></dc:rights>
-<?php if (!empty($r['abstract'])): ?>
-                    <dc:description><?= esc($r['abstract']) ?></dc:description>
-<?php endif; ?>
-<?php if (!empty($r['doi'])): ?>
-                    <dc:identifier><?= esc($r['doi']) ?></dc:identifier>
-<?php endif; ?>
-                    <dc:identifier><?= esc($r['url']) ?></dc:identifier>
-                </oai_dc:dc>
-            </metadata>
-        </record>
-<?php endforeach; ?>
+        <?php foreach ($records as $r): ?>
+            <record>
+                <header>
+                    <identifier><?= esc($r['id']) ?></identifier>
+                    <datestamp><?= esc($r['date']) ?></datestamp>
+                </header>
+                <metadata>
+                    <oai_dc:dc xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/"
+                        xmlns:dc="http://purl.org/dc/elements/1.1/"
+                        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                        xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd">
+                        <dc:title><?= esc($r['title']) ?></dc:title>
+                        <?php foreach ($r['creators'] as $c): ?>
+                            <dc:creator><?= esc($c) ?></dc:creator>
+                        <?php endforeach; ?>
+                        <?php if (!empty($r['subject'])): ?>
+                            <dc:subject><?= esc($r['subject']) ?></dc:subject>
+                        <?php endif; ?>
+                        <?php if (!empty($r['publisher'])): ?>
+                            <dc:publisher><?= esc($r['publisher']) ?></dc:publisher>
+                        <?php endif; ?>
+                        <?php if (!empty($r['pubDate'])): ?>
+                            <dc:date><?= esc($r['pubDate']) ?></dc:date>
+                        <?php endif; ?>
+                        <?php if (!empty($r['type'])): ?>
+                            <dc:type><?= esc($r['type']) ?></dc:type>
+                        <?php endif; ?>
+                        <?php if (!empty($r['language'])): ?>
+                            <dc:language><?= esc($r['language']) ?></dc:language>
+                        <?php endif; ?>
+                        <?php if (!empty($r['rights'])): ?>
+                            <dc:rights><?= esc($r['rights']) ?></dc:rights>
+                        <?php endif; ?>
+                        <?php if (!empty($r['abstract'])): ?>
+                            <dc:description><?= esc($r['abstract']) ?></dc:description>
+                        <?php endif; ?>
+                        <?php if (!empty($r['doi'])): ?>
+                            <dc:identifier><?= esc($r['doi']) ?></dc:identifier>
+                        <?php endif; ?>
+                        <dc:identifier><?= esc($r['url']) ?></dc:identifier>
+                    </oai_dc:dc>
+                </metadata>
+            </record>
+        <?php endforeach; ?>
     </ListRecords>
 </OAI-PMH>

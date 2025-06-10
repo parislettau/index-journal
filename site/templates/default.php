c880b105b40f120aff8b4541bc4b64a850e7efec
parislettau <55466163+parislettau@users.noreply.github.com>
2025-06-10 13:21:03 +1000

    Update default.php

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,8 +1,16 @@
 <?php snippet('header') ?>
+
 <main data-template="<?= $page->template() ?>">
+  <header>
+    <!-- Title -->
+    <section class="title-block" style="background-color: rgb(<?= $page->parent()->issue_color() ?>)">
+      <h1 style="font-size:6vw; display:block" style="">
+        <span class="title"><?= $page->title() ?></span>
+      </h1>
+    </section>
+  </header>
   <div class="text-block">
     <?= $page->text()->kirbytext() ?>
   </div>
 </main>
 <?php snippet('footer') ?>
-
